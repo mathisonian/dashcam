c0b97ab689fe14be4ea39ef41ce52c584ab57663
Matthew Conlen <mc@mathisonian.com>
2017-03-19 16:07:48 -0700

    updates

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
-# consumer-complaints
-An exploration of the complaints database from the Consumer Financial Protection Bureau
+# dashcam
+
--- components/annotated-svg.js
+++ components/annotated-svg.js
@@ -1,22 +1,22 @@
 const React = require('react');
 const IdyllComponent = require('idyll-component');
 const InlineSVG = require('react-inlinesvg');
-const css = require('sheetify');
-
+const ReactDOM = require('react-dom');
+const d3 = require('d3-selection');
 
 class SVG extends IdyllComponent {
   render() {
-
-    const { showAnnotations, src } = this.props;
-    // const cssString = 
-    // const prefix = css`
-    //   :host .annotation {
-    //     opacity: ${showAnnotations ? 1 : 0};
-    //   }
-    // `;
+    const { showAnnotations, src, r } = this.props;
+    if (showAnnotations && this.node) {
+      this.node.select('.annotations').style('opacity', 1);
+    } else if (this.node) {
+      this.node.select('.annotations').style('opacity', 0);
+    }
 
     return (
-      <InlineSVG className={''} src={src} />
+      <div>
+        <InlineSVG className={''} src={src} ref={(node) => { this.node = d3.select(ReactDOM.findDOMNode(node)); }} />
+      </div>
     );
   }
 }
